578873b4ad9e6c3c3ac965cf8880080d7c9bbf26
snapshotpl <witold@wasiczko.pl>
2015-11-30 22:41:33 +0100

    Working code

---
+++ src/MaintenanceMiddleware.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace PhpMiddleware\Maintenance;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class MaintenanceMiddleware
+{
+    /**
+     * @var string
+     */
+    private $retryAfter = '';
+
+    /**
+     * @var int
+     */
+    private $refresh = 0;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return \self
+     */
+    public static function create()
+    {
+        return new self();
+    }
+
+    /**
+     * @param int $seconds
+     *
+     * @return self
+     */
+    public static function createWithRetryAsSeconds($seconds)
+    {
+        if (!is_int($seconds)) {
+            throw new \InvalidArgumentException('Seconds must be integer');
+        }
+
+        $instance = new self();
+
+        $instance->retryAfter = (string) $seconds;
+
+        return $instance;
+    }
+
+    /**
+     * @param int $seconds
+     *
+     * @return self
+     */
+    public function createWithRetryAsSecondsAndRefresh($seconds)
+    {
+        $instance = self::createWithRetryAsSeconds($seconds);
+        $instance->refresh = $seconds;
+
+        return $instance;
+    }
+
+    /**
+     * @param \DateTimeInterface $datetime
+     *
+     * @return self
+     */
+    public static function createWithRetryAsDateTime(\DateTimeInterface $datetime)
+    {
+        $instance = new self();
+
+        $instance->retryAfter = $datetime->format(\DateTime::RFC2822);
+
+        return $instance;
+    }
+
+    /**
+     * @param \DateTimeInterface $datetime
+     *
+     * @return self
+     */
+    public static function createWithRetryAsDateTimeAndRefresh(\DateTimeInterface $datetime)
+    {
+        $instance = self::createWithRetryAsDateTime($datetime);
+        $diff = time() - $datetime->getTimestamp();
+        $instance->refresh = $diff;
+
+        return $instance;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param callable $next
+     *
+     * @return ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        if ($this->retryAfter !== '') {
+            $response = $response->withHeader('Retry-After', $this->retryAfter);
+
+            if ($this->refresh > 0) {
+                $response = $response->withHeader('Refresh', (string) $this->refresh);
+            }
+        }
+
+        return $response->withStatus(503);
+    }
+}
--- test/MaintenanceMiddlewareTest.php
+++ test/MaintenanceMiddlewareTest.php
@@ -2,12 +2,105 @@
 
 namespace PhpMiddlewareTest\Maintenance;
 
-/**
- * MaintenanceMiddlewareTest
- *
- * @author Witold Wasiczko <witold@wasiczko.pl>
- */
-class MaintenanceMiddlewareTest
-{
+use DateTime;
+use Exception;
+use PhpMiddleware\Maintenance\MaintenanceMiddleware;
+use PHPUnit_Framework_TestCase;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequest;
 
+class MaintenanceMiddlewareTest extends PHPUnit_Framework_TestCase
+{
+    public function testWithoutRetry()
+    {
+        $request = new ServerRequest();
+        $response = new Response();
+
+        $middleware = MaintenanceMiddleware::create();
+
+        $next = function() {
+            throw new Exception('Next should not be called');
+        };
+
+        $response = $middleware->__invoke($request, $response, $next);
+
+        $this->assertSame(503, $response->getStatusCode());
+        $this->assertSame('', $response->getHeaderLine('Retry-After'));
+    }
+
+    public function testWithRetryAsSeconds()
+    {
+        $request = new ServerRequest();
+        $response = new Response();
+
+        $middleware = MaintenanceMiddleware::createWithRetryAsSeconds(3600);
+
+        $next = function() {
+            throw new Exception('Next should not be called');
+        };
+
+        $response = $middleware->__invoke($request, $response, $next);
+
+        $this->assertSame(503, $response->getStatusCode());
+        $this->assertSame('3600', $response->getHeaderLine('Retry-After'));
+        $this->assertSame('', $response->getHeaderLine('Refresh'));
+    }
+
+    public function testWithRetryAsSecondsWithRefresh()
+    {
+        $request = new ServerRequest();
+        $response = new Response();
+
+        $middleware = MaintenanceMiddleware::createWithRetryAsSecondsAndRefresh(3600);
+
+        $next = function() {
+            throw new Exception('Next should not be called');
+        };
+
+        $response = $middleware->__invoke($request, $response, $next);
+
+        $this->assertSame(503, $response->getStatusCode());
+        $this->assertSame('3600', $response->getHeaderLine('Retry-After'));
+        $this->assertSame('3600', $response->getHeaderLine('Refresh'));
+    }
+
+    public function testWithRetryAsDatetime()
+    {
+        $request = new ServerRequest();
+        $response = new Response();
+
+        $date = DateTime::createFromFormat('Y-m-d H:i:s', '2015-11-30 11:12:13');
+
+        $middleware = MaintenanceMiddleware::createWithRetryAsDateTime($date);
+
+        $next = function() {
+            throw new Exception('Next should not be called');
+        };
+
+        $response = $middleware->__invoke($request, $response, $next);
+
+        $this->assertSame(503, $response->getStatusCode());
+        $this->assertSame('Mon, 30 Nov 2015 11:12:13 +0000', $response->getHeaderLine('Retry-After'));
+        $this->assertSame('', $response->getHeaderLine('Refresh'));
+    }
+
+    public function testWithRetryAsDatetimeWithRefresh()
+    {
+        $request = new ServerRequest();
+        $response = new Response();
+
+        $date = DateTime::createFromFormat('Y-m-d H:i:s', '2015-11-30 11:12:13');
+
+        $middleware = MaintenanceMiddleware::createWithRetryAsDateTimeAndRefresh($date);
+
+        $next = function() {
+            throw new Exception('Next should not be called');
+        };
+
+        $response = $middleware->__invoke($request, $response, $next);
+
+        $this->assertSame(503, $response->getStatusCode());
+        $this->assertSame('Mon, 30 Nov 2015 11:12:13 +0000', $response->getHeaderLine('Retry-After'));
+        $this->assertGreaterThan(1000, (int) $response->getHeaderLine('Refresh'));
+    }
 }
